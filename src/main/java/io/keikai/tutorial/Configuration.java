623ed0fc568eed380a44a045dac576dbf9c3e35b
hawkchen <hawkchen@potix.com>
2018-05-17 16:38:05 +0800

    fix filter

---
+++ src/main/java/io/keikai/tutorial/Configuration.java
@@ -0,0 +1,5 @@
+package io.keikai.tutorial;
+
+public class Configuration {
+    public static final String INTERNAL_KEIKAI_SERVER = "http://localhost:8888";
+}
--- src/main/java/io/keikai/tutorial/EditorFilter.java
+++ src/main/java/io/keikai/tutorial/EditorFilter.java
@@ -7,7 +7,8 @@ import javax.servlet.annotation.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-@WebFilter("/editor.html")
+@WebFilter(filterName = "TimeOfDayFilter",
+        urlPatterns = {"/editor.jsp"})
 public class EditorFilter implements Filter {
     private Spreadsheet spreadsheet;
     static public String SPREADSHEET = "spreadsheet"; //the key to store Spreadsheet component
@@ -21,13 +22,15 @@ public class EditorFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         initSpreadsheet(request);
+        chain.doFilter(request, response);
     }
-    
+
     private void initSpreadsheet(ServletRequest request) {
         spreadsheet = Keikai.newClient(Configuration.INTERNAL_KEIKAI_SERVER);
-        String jsAPI = spreadsheet.getURI("spreadsheet"); // pass the anchor DOM element id for rendering keikai
-        request.setAttribute("jsAPI", jsAPI);
+        String keikaiJs = spreadsheet.getURI("spreadsheet"); // pass the anchor DOM element id for rendering keikai
+        request.setAttribute("keikaiJs", keikaiJs);
         ((HttpServletRequest)request).getSession().setAttribute(SPREADSHEET, spreadsheet);
+        System.out.println(keikaiJs);
     }
 
     @Override
