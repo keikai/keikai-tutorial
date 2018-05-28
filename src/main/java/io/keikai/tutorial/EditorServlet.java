0a5b0373ecffb49dd6287c4bba1d8ff9abdd57ff
hawkchen <hawkchen@potix.com>
2018-05-28 18:27:55 +0800

    load a default file at the beginning

--- src/main/java/io/keikai/tutorial/EditorServlet.java
+++ src/main/java/io/keikai/tutorial/EditorServlet.java
@@ -12,8 +12,10 @@ import java.util.concurrent.ExecutionException;
 @WebServlet("/editor/*")
 public class EditorServlet extends BaseServlet {
     private static final String BLANK_XLSX = "blank.xlsx";
+    private static final String DEFAULT_XLSX = "welcome.xlsx";
     private File defaultExportFolder;
     private File defaultBookFolder;
+    private File defaultFile;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -21,6 +23,8 @@ public class EditorServlet extends BaseServlet {
         defaultExportFolder = new File(getServletContext().getRealPath(Configuration.DEFAULT_FILE_FOLDER));
         defaultBookFolder = new File(getServletContext().getRealPath("/WEB-INF" +
                 File.separator + Configuration.INTERNAL_FILE_FOLDER + File.separator + BLANK_XLSX));
+        defaultFile = new File(getServletContext().getRealPath("/WEB-INF" +
+                File.separator + Configuration.INTERNAL_FILE_FOLDER + File.separator + DEFAULT_XLSX));
 //        Configuration.enableSocketIOLog();
     }
 
@@ -61,42 +65,30 @@ public class EditorServlet extends BaseServlet {
     @Override
     protected void initSpreadsheet(ServletRequest request) {
         super.initSpreadsheet(request);
-        registerToolbarButtonsHandlers();
+        addToolbarEventListeners();
+        spreadsheet.imports(DEFAULT_XLSX, defaultFile);
     }
 
-    protected void registerToolbarButtonsHandlers() {
+
+    protected void addToolbarEventListeners() {
         ExceptionalConsumer listener = (event) -> {
             final AuxActionEvent auxActionEvent = (AuxActionEvent) event;
             final String action = auxActionEvent.getAction();
             if (action.equals("saveBook")) {
 
-            }else if (action.equals("newBook")){
-                spreadsheet.containsWorkbook(BLANK_XLSX).thenAccept(existed ->{
+            } else if (action.equals("newBook")) {
+                spreadsheet.containsWorkbook(BLANK_XLSX).thenAccept(existed -> {
                     spreadsheet.deleteWorkbook(BLANK_XLSX);
                     spreadsheet.imports(BLANK_XLSX, defaultBookFolder).exceptionally(throwable -> {
                         throwable.printStackTrace();
                         return null;
                     });
                 });
-            }else if (action.equals("exportToFile")){
+            } else if (action.equals("exportToFile")) {
 
             }
-            System.out.println(action);
         };
 
-        ExceptionableFunction listener2 = (event) -> {
-            final AuxActionEvent auxActionEvent = (AuxActionEvent) event;
-            final String action = auxActionEvent.getAction();
-            final Range range = auxActionEvent.getActiveCell();
-            if (!action.equals("saveBook")) {
-
-            }
-            System.out.println(action);
-            range.loadWorkbook().thenAcceptBoth(range.loadWorksheet(), (Workbook workbook, Worksheet worksheet) -> {
-                System.out.println("Save [ " + workbook.getName() + "] " + worksheet.getName());
-            });
-            return null;
-        };
         spreadsheet.addEventListener(Events.ON_AUX_ACTION, listener::accept);
 
     }
