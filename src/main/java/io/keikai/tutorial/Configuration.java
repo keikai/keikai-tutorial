7f8b129c45ca33565e75bb0f5ab57195ccea5767
hawkchen <hawkchen@potix.com>
2018-05-28 16:17:39 +0800

    add socket io log

--- src/main/java/io/keikai/tutorial/Configuration.java
+++ src/main/java/io/keikai/tutorial/Configuration.java
@@ -1,6 +1,7 @@
 package io.keikai.tutorial;
 
 import java.io.File;
+import java.util.logging.*;
 
 public class Configuration {
     public static final String INTERNAL_KEIKAI_SERVER = "http://localhost:8888";
@@ -8,4 +9,15 @@ public class Configuration {
     public static final String DEFAULT_FILE_FOLDER = "export";
     public static final String INTERNAL_FILE_FOLDER = "book";
     public static final String KEIKAI_JS = "keikaiJs";
+
+
+    static public void enableSocketIOLog() {
+        Logger log = java.util.logging.Logger.getLogger("");
+        log.setLevel(Level.FINER);
+
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter());
+        handler.setLevel(Level.ALL);
+        log.addHandler(handler);
+    }
 }
