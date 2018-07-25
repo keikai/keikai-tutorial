5d719148a4a3cf6ea1aa15df880a26ecd2966d85
hawkchen <hawkchen@potix.com>
2018-07-25 16:25:44 +0800

    update with synchronous API

--- src/main/java/io/keikai/tutorial/MyApp.java
+++ src/main/java/io/keikai/tutorial/MyApp.java
@@ -4,7 +4,6 @@ import io.keikai.client.api.*;
 import io.keikai.client.api.event.*;
 
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * implement the application logic
@@ -17,86 +16,80 @@ public class MyApp {
     public MyApp(Spreadsheet spreadsheet) {
         this.spreadsheet = spreadsheet;
         addEventListeners();
-        fillExpense();
+        loadExpenseToSheet();
     }
 
-    private void fillExpense() {
+    private void loadExpenseToSheet() {
         List<Expense> list = SampleDataDao.queryByCategory();
         int row = 18;
         int col = 0;
         for (Expense expense : list) {
             Range categoryCell = spreadsheet.getRange(row, col);
-            categoryCell.applyValue(expense.getCategory());
+            categoryCell.setValue(expense.getCategory());
             Range quantityCell = spreadsheet.getRange(row, col + 1);
-            quantityCell.applyValue(expense.getQuantity());
+            quantityCell.setValue(expense.getQuantity());
             Range subtotalCell = spreadsheet.getRange(row, col + 2);
-            subtotalCell.applyValue(expense.getSubtotal());
+            subtotalCell.setValue(expense.getSubtotal());
             row++;
         }
     }
 
     private void addEventListeners() {
         ExceptionalConsumer<RangeEvent> listener = (event) -> {
-            spreadsheet.loadActiveWorksheet().thenAccept(worksheet -> {
-//            event.getRange().loadWorksheet().thenAccept(worksheet -> {
-                if (isAddButtonClicked(worksheet.getIndex(), event.getRange())) {
-                    spreadsheet.applyActiveWorksheet(1);
-                    spreadsheet.setCurrentWorksheet(1); //apply active worksheet doesn't set current worksheet accordingly
-                } else if (isDoneButtonClicked(worksheet.getIndex(), event.getRange())) {
-                    addExpense();
-                }
-            });
+            if (isAddButtonClicked(event.getWorksheet().getIndex(), event.getRange())) {
+                spreadsheet.setActiveWorksheet(1); //switch to expense sheet
+            } else if (isDoneButtonClicked(event.getWorksheet().getIndex(), event.getRange())) {
+                addExpense();
+                spreadsheet.setActiveWorksheet(0);
+                loadExpenseToSheet();
+            }
         };
         spreadsheet.addEventListener(Events.ON_CELL_CLICK, listener::accept);
     }
 
     /**
-     * read expense in a specific range
+     * save expense records in a specific range
      */
     private void addExpense() {
-        List<CompletableFuture> futures = new LinkedList<>();
-        for (int rowIndex = STARTING_ROW ; rowIndex < STARTING_ROW + 4 ; rowIndex++) {
-            CompletableFuture readFuture = readExpense(rowIndex, CATEGORY_COLUMN).thenAccept(expense -> {
-                if (validate(expense)) {
-                    SampleDataDao.insert(expense);
-                }
-            });
-            futures.add(readFuture);
+        for (int rowIndex = STARTING_ROW; rowIndex < STARTING_ROW + 4; rowIndex++) {
+            Expense expense = readExpense(rowIndex, CATEGORY_COLUMN);
+            if (validate(expense)) {
+                SampleDataDao.insert(expense);
+            } else {
+                break;
+            }
         }
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).thenRun(() -> {
-            spreadsheet.setCurrentWorksheet(0) //apply active worksheet doesn't set current worksheet accordingly
-                .thenRun(() -> {
-                    fillExpense();
-                });
-            clearExpense();
-            spreadsheet.applyActiveWorksheet(0);
-        });
+        clearExpense();
     }
 
 
     private boolean validate(Expense expense) {
-        return expense.getCategory()!= null;
+        return expense.getCategory() != null;
     }
 
     private void clearExpense() {
         System.out.println("clear");
     }
 
-    private CompletableFuture<Expense> readExpense(int row, int col) {
+    private Expense readExpense(int row, int col) {
         Expense expense = new Expense();
-        CompletableFuture<RangeValue> categoryLoader = spreadsheet.getRange(row, col).loadValue();
-        categoryLoader.thenAccept(rangeValue -> {
-            expense.setCategory(rangeValue.getCellValue().getStringValue());
+        expense.setCategory(spreadsheet.getRange(row, col).getValue());
+        getCellValue(spreadsheet.getRange(row, col + 1)).ifPresent(cellValue -> {
+            expense.setQuantity(cellValue.getDoubleValue().intValue());
         });
-        CompletableFuture<RangeValue> quantityLoader = spreadsheet.getRange(row, col + 1).loadValue();
-        quantityLoader.thenAccept(rangeValue -> {
-            expense.setQuantity(rangeValue.getCellValue().getDoubleValue().intValue());
+        getCellValue(spreadsheet.getRange(row, col + 3)).ifPresent(cellValue -> {
+            expense.setSubtotal(cellValue.getDoubleValue().intValue());
         });
-        CompletableFuture<RangeValue> subtotalLoader = spreadsheet.getRange(row, col + 3).loadValue();
-        subtotalLoader.thenAccept(rangeValue -> {
-            expense.setSubtotal(rangeValue.getCellValue().getDoubleValue().intValue());
-        });
-        return CompletableFuture.allOf(categoryLoader, subtotalLoader).thenApply(aVoid -> expense);
+        return expense;
+    }
+
+    private Optional<CellValue> getCellValue(Range range){
+        CellValue value = range.getRangeValue().getCellValue();
+        if (range.getValue() == null){
+            return Optional.empty();
+        }else{
+            return Optional.of(value);
+        }
     }
 
     private boolean isAddButtonClicked(int sheetIndex, Range range) {
