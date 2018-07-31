2c1a943b10561bf423722328ab6437eb76d05dfc
hawkchen <hawkchen@potix.com>
2018-07-31 15:34:33 +0800

    remove unused code and blank.xlsx (implemented by toolbar)

--- src/main/java/io/keikai/tutorial/web/EditorServlet.java
+++ src/main/java/io/keikai/tutorial/web/EditorServlet.java
@@ -12,42 +12,23 @@ import java.io.*;
 
 @WebServlet("/editor/*")
 public class EditorServlet extends BaseServlet {
-    private static final String BLANK_XLSX = "blank.xlsx";
     private static final String DEFAULT_XLSX = "welcome.xlsx";
-    private File defaultExportFolder;
-    private File defaultBookFolder;
     private File defaultFile;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        defaultExportFolder = new File(getServletContext().getRealPath(Configuration.DEFAULT_FILE_FOLDER));
-        defaultBookFolder = new File(getServletContext().getRealPath("/WEB-INF" +
-                File.separator + Configuration.INTERNAL_FILE_FOLDER + File.separator + BLANK_XLSX));
         defaultFile = new File(getServletContext().getRealPath("/WEB-INF" +
                 File.separator + Configuration.INTERNAL_FILE_FOLDER + File.separator + DEFAULT_XLSX));
 //        Configuration.enableSocketIOLog();
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        spreadsheet = getSpreadsheet(req);
-        Result result = Result.getSuccess("success");
-        resp.setContentType("text/plain");
-        try {
-        } catch (Exception e) {
-            e.printStackTrace();
-            result = Result.getErrorResult(e.toString());
-        }
-        resp.getWriter().print(gson.toJson(result));
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         initSpreadsheet(req);
         try {
             spreadsheet.imports(DEFAULT_XLSX, defaultFile);
-            addToolbarEventListeners();
         } catch (DuplicateNameException e) {
             throw new IOException(e);
         } catch (AbortedException e) {
@@ -56,24 +37,4 @@ public class EditorServlet extends BaseServlet {
         req.getRequestDispatcher("/editor.jsp").forward(req, resp);
     }
 
-    protected void addToolbarEventListeners() {
-        ExceptionalConsumer listener = (event) -> {
-            final AuxActionEvent auxActionEvent = (AuxActionEvent) event;
-            final String action = auxActionEvent.getAction();
-            if (action.equals("saveBook")) {
-
-            }
-//            else if (action.equals(AuxAction.NEW_BOOK.getAction())) {
-//                spreadsheet.containsWorkbook(BLANK_XLSX).thenAccept(existed -> {
-//                    spreadsheet.deleteWorkbook(BLANK_XLSX);
-//                    spreadsheet.imports(BLANK_XLSX, defaultBookFolder).exceptionally(throwable -> {
-//                        throwable.printStackTrace();
-//                        return null;
-//                    });
-//                });
-//            }
-        };
-
-        spreadsheet.addEventListener(Events.ON_AUX_ACTION, listener::accept);
-    }
 }
