a32499f11bddcfdef13b87322f5e4eecb31255fa
hawkchen <hawkchen@potix.com>
2018-08-02 16:24:58 +0800

    remove unused class

--- src/main/java/io/keikai/tutorial/Configuration.java
+++ src/main/java/io/keikai/tutorial/Configuration.java
@@ -1,12 +1,10 @@
 package io.keikai.tutorial;
 
-import java.io.File;
 import java.util.logging.*;
 
 public class Configuration {
     public static final String DEFAULT_KEIKAI_SERVER = "http://localhost:8888";
     public static final String SPREADSHEET = "spreadsheet"; //the key to store Spreadsheet component
-    public static final String DEFAULT_FILE_FOLDER = "export";
     public static final String INTERNAL_FILE_FOLDER = "book";
     public static final String KEIKAI_JS = "keikaiJs";
 
