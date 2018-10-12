7ddc351d8395fd31deb6d9a9867a1c2e3bb585c7
hawkchen <hawkchen@potix.com>
2018-10-12 19:26:27 +0800

    rename form template files

--- src/main/java/io/keikai/tutorial/web/AppContextListener.java
+++ src/main/java/io/keikai/tutorial/web/AppContextListener.java
@@ -22,7 +22,7 @@ public class AppContextListener implements ServletContextListener {
 
     static private void loadFormList(ServletContext context) {
         formList.add(new File(context.getRealPath(Configuration.getDefaultFileFolder() + "Leave Application.xlsx")));
-        formList.add(new File(context.getRealPath(Configuration.getDefaultFileFolder() + "Business Trip.xlsx")));
+        formList.add(new File(context.getRealPath(Configuration.getDefaultFileFolder() + "Business Trip Request.xlsx")));
     }
 
     @Override
