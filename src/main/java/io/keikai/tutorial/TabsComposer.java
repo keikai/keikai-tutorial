b3af7b681f01bc7e85d989723a96b07cc8f023c4
hawkchen <hawkhero@gmail.com>
2021-08-05 15:46:41 +0800

    update according to use case

--- src/main/java/io/keikai/tutorial/TabsComposer.java
+++ src/main/java/io/keikai/tutorial/TabsComposer.java
@@ -3,45 +3,32 @@ package io.keikai.tutorial;
 import io.keikai.api.*;
 import io.keikai.api.model.Sheet;
 import io.keikai.ui.Spreadsheet;
+import io.keikai.ui.event.*;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.select.annotation.*;
 
 public class TabsComposer extends SelectorComposer<Component> {
     @Wire
     private Spreadsheet spreadsheet;
-    static final String ACTIVE_COLOR = "#00b0f0";
-
-    @Override
-    public void doAfterCompose(Component comp) throws Exception {
-        super.doAfterCompose(comp);
-        //put initialization logic here
-        init();
-    }
 
     /**
      * hide/unhide children sheets
+     * https://doc.keikai.io/dev-ref/handling_events/Sheet_Event
      */
-    private void init() {
+    @Listen(Events.ON_SHEET_SELECT + "= #spreadsheet")
+    public void showGroupSheet(SheetSelectEvent event){
         int nSheet = spreadsheet.getBook().getNumberOfSheets();
-        boolean visible = true;
-        for (int i = 0 ; i < nSheet ; i++){
-            Sheet sheet = spreadsheet.getBook().getSheetAt(i);
-            if (ACTIVE_COLOR.equals(sheet.getInternalSheet().getTabColor())
-            && !"Cable TV".equals(sheet.getSheetName())){
-                if (sheet.isHidden()){
-                    Ranges.range(sheet).setSheetVisible(Range.SheetVisible.VISIBLE);
-                    visible = true;
-                }else{
-                    Ranges.range(sheet).setSheetVisible(Range.SheetVisible.HIDDEN);
-                    visible = false;
-                }
+        String activeColor = event.getSheet().getInternalSheet().getTabColor();
+        for (int i = 4 ; i < nSheet ; i++) {
+            Sheet childSheet = spreadsheet.getBook().getSheetAt(i);
+            if (childSheet.getInternalSheet().getTabColor().equals(activeColor)){
+                if (childSheet.isHidden())
+                    Ranges.range(childSheet).setSheetVisible(Range.SheetVisible.VISIBLE);
+            }else{
+                if (!childSheet.isHidden())
+                    Ranges.range(childSheet).setSheetVisible(Range.SheetVisible.HIDDEN);
             }
-        }
-        if (visible){
-            spreadsheet.setSelectedSheet("Spectrum");
-        }else{
-            spreadsheet.setSelectedSheet("Index");
         }
     }
 }
